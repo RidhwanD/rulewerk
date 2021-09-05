18deb79afb0201bab4fc7e381e8edd7165abe4a2
Ridhwan Dewoprabowo <ridhwan.dewoprabowo@gmail.com>
2021-09-05 10:46:25 +0200

    Added some validity checks for augmentated meta-rules.

--- rulewerk-synthesis/src/main/java/org/semanticweb/rulewerk/synthesis/RuleGenerator.java
+++ rulewerk-synthesis/src/main/java/org/semanticweb/rulewerk/synthesis/RuleGenerator.java
@@ -1,14 +1,19 @@
 package org.semanticweb.rulewerk.synthesis;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.semanticweb.rulewerk.core.model.api.Literal;
 import org.semanticweb.rulewerk.core.model.api.PositiveLiteral;
 import org.semanticweb.rulewerk.core.model.api.Predicate;
 import org.semanticweb.rulewerk.core.model.api.Rule;
+import org.semanticweb.rulewerk.core.model.api.Term;
+import org.semanticweb.rulewerk.core.model.api.Variable;
 import org.semanticweb.rulewerk.core.model.implementation.Expressions;
 
 public class RuleGenerator {
@@ -41,6 +46,58 @@ public class RuleGenerator {
 		return res;
 	}
 	
+	public boolean varClauseRepeat(Rule metaR) {
+		// Check whether in meta-rule r, a variable are repeated in a clause, e.g. Edge(?v0, ?v0)
+		// Assumption: a meta-rule r does not contain any constant in its arguments.
+		for (Literal l : metaR.getHead()) {
+			List<Variable> headVars = l.getVariables().toList();
+			if (headVars.size() < l.getPredicate().getArity()) return true;
+		}
+		for (Literal l : metaR.getBody()) {
+			List<Variable> bodyVars = l.getVariables().toList();
+			if (bodyVars.size() < l.getPredicate().getArity()) return true;
+		}
+		return false;
+	}
+	
+	public boolean varAppearsOnce(Rule metaR) {
+		// Check whether in meta-rule r, a variable appears exactly once.
+		// Assumption: a meta-rule r does not contain any constant in its arguments.
+		List<Variable> vars = metaR.getVariables().toList();
+		List<Term> occurenceVars = new ArrayList<>();
+		for (Literal l : metaR.getHead()) {
+			occurenceVars.addAll(l.getArguments());
+		}
+		for (Literal l : metaR.getBody()) {
+			occurenceVars.addAll(l.getArguments());
+		}
+		for (Variable v : vars) {
+			if (Collections.frequency(occurenceVars, v) == 1) return true;
+		}
+		return false;
+	}
+	
+	public boolean reoccurClause(Rule metaR) {
+		// Check whether in meta-rule r, two clauses share same set of variables in same order.
+		// Assumption: a meta-rule r does not contain any constant in its arguments.
+		
+		// In combination?
+		List<Literal> clauses = new ArrayList<>(metaR.getBody().getLiterals());
+		clauses.addAll(metaR.getHead().getLiterals());
+		Set<Literal> clausesSet = new HashSet<>(clauses);
+		if (clausesSet.size() < clauses.size()) return true;
+		
+		// Or separated by head and body?
+		List<Literal> headClauses = new ArrayList<>(metaR.getHead().getLiterals());
+		Set<Literal> headClausesSet = new HashSet<>(headClauses);
+		if (headClausesSet.size() < headClauses.size()) return true;
+		List<Literal> bodyClauses = new ArrayList<>(metaR.getBody().getLiterals());
+		Set<Literal> bodyClausesSet = new HashSet<>(bodyClauses);
+		if (bodyClausesSet.size() < bodyClauses.size()) return true;
+		
+		return false;
+	}
+		
 	public List<Rule> simpleGenerator() {
 		// Simple generator: assume meta rule set has the form P1(x0,...,xn) :- P2(..), ..., Pm(...).
 		Map<Integer,List<List<Predicate>>> generatedPerm = new HashMap<>();
